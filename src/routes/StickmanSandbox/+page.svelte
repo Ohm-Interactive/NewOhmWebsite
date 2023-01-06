ea68617559f2263c13870733dfd19fe631a728ac
Twingamerdudes <gameromgtwins@gmail.com>
2023-01-06 11:41:39 -0500

    Updated some info for Stickman Sandbox

--- src/routes/StickmanSandbox/+page.svelte
+++ src/routes/StickmanSandbox/+page.svelte
@@ -3,7 +3,9 @@
     <img src="StickmanSandbox.png" alt="Stickman Sandbox">
     <p>This is Stickman Sandbox, a sandbox game where you play as a Stickman.</p>
     <p>How did you gain these godly powers though, well, you touched grass of course.</p>
-    <h2 style="color: #FFD700; font-size: 2rem;">WORK IN PROGRESS</h2>
+    <p>Final release will include modding support along with custom maps and lots of cool items.</p>
+    <a href="https://ohm-interactive.itch.io/stickman-sandbox" class="disbutton" style="font-size: 2rem;">Check out the alpha build here!</a>
+    <h2 style="color: #FFD700; font-size: 2rem;">PROJECT IS IN ALPHA</h2>
 </div>
 <style>
     .Content{
